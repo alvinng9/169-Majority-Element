24450801e7db937e8890be254466296254699c14
Alvin <alvinngg9@gmail.com>
2024-08-15 17:34:57 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 public class Solution {
     public int majorityElement(int[] nums) {
+        //sort the array from least to most
         Arrays.sort(nums);
-        return nums[nums.length / 2];
+        //return the value of the index in the middle
+        //since the majority element will appear more than half of the indexes
+        //it is guarantee that the majority element will be in the middle
+        return nums[nums.length/2];
     }
 }
